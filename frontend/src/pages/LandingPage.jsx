1081252051054de8c860e317319acbcd6a8ce6f6
JamesVictor-o <victorjames40v@gmail.com>
2024-12-14 13:17:05 +0100

    made changes

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <section class="bg-white bg-[url('src/assets/man-handshaking-his-employer-after-being-accepted-his-new-office-job.jpg')] bg-cover h-screen pt-36 ">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-2xl xl:text-6xl dark:text-black">Simplify Employee Onboarding, Amplify Team Success</h1>
-            <p class="max-w-2xl mb-6  text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-black font-medium mt-6">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
-             <Link to={"register"} class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg bg-blue-800 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Register
-             </Link>
-          
+          <h1 class="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-2xl xl:text-6xl dark:text-black">
+            Simplify Employee Onboarding, Amplify Team Success
+          </h1>
+          <p class="max-w-2xl mb-6  text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-black font-medium mt-6">
+            From checkout to global sales tax compliance, companies around the
+            world use Flowbite to simplify their payment stack.
+          </p>
+          <Link
+            to={"register"}
+            class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg bg-blue-800 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            Register
+          </Link>
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"> */}
-        </div>                
-    </div>
+          {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"> */}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
